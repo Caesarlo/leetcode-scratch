ec05e4ee07f0ab92cd772ae2b8aa839f590592bb
GUMP <350960338@qq.com>
2026-03-26 16:38:20 +0800

    2026-3-26

--- py/l134.py
+++ py/l134.py
@@ -1,6 +1,34 @@
 from typing import List
 
 
+class Solution0:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0
+        tank = 0
+        start = 0
+
+        for i in range(len(gas)):
+            diff = gas[i]-cost[i]
+            total += diff
+            tank += diff
+
+            if tank < 0:
+                tank = 0
+                start = i+1
+
+        return start if total >= 0 else -1
+
+
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        ...
+        spare = 0
+        minSpare = float('inf')
+        minIndex = 0
+
+        for i in range(len(gas)):
+            spare += gas[i]-cost[i]
+            if spare < minSpare:
+                minSpare = spare
+                minIndex = i
+
+        return (minIndex+1) % len(gas) if spare >= 0 else -1
